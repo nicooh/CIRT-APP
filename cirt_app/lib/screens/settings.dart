171c75e8a3c6399b9f26c603e337925b0c58cdf7
nicooh <nik.ciano009@gmail.com>
2021-05-11 18:59:55 +0200

    modifica1900

--- cirt_app/lib/screens/settings.dart
+++ cirt_app/lib/screens/settings.dart
@@ -14,7 +14,7 @@ class Settings extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
         title: Text(
-          'Setting',
+          'Settings',
           style: TextStyle(color: Colors.black),
         ),
       ),
